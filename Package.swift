e79dfd90737d5e11b53a7e6f3db514d7de4e422d
fto-k <fto@kovalee.app>
2023-07-28 16:13:00 +0200

    moved RemoteConfig and AB tests functions for KovaleeSDK to specific Swift Package

--- Package.swift
+++ Package.swift
@@ -1,28 +1,34 @@
-// swift-tools-version: 5.8
+// swift-tools-version: 5.5
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "KovaleeRemoteConfig",
+	defaultLocalization: "en",
+	platforms: [
+		.iOS(.v14)
+	],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "KovaleeRemoteConfig",
-            targets: ["KovaleeRemoteConfig"]),
+		.library(
+			name: "KovaleeRemoteConfig",
+			targets: [
+				"KovaleeRemoteConfig",
+			]
+		),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+		.package(url: "https://github.com/cotyapps/Kovalee-iOS-SDK", branch: "sdk-spm-split"),
+		.package(url: "https://github.com/firebase/firebase-ios-sdk.git", from: Version(10, 12, 0)),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "KovaleeRemoteConfig",
-            dependencies: []),
-        .testTarget(
-            name: "KovaleeRemoteConfigTests",
-            dependencies: ["KovaleeRemoteConfig"]),
+		.target(
+			name: "KovaleeRemoteConfig",
+			dependencies: [
+				.product(name: "KovaleeSDK", package: "Kovalee-iOS-SDK"),
+				.product(name: "FirebaseAnalyticsSwift", package: "firebase-ios-sdk"),
+				.product(name: "FirebaseRemoteConfigSwift", package: "firebase-ios-sdk")
+			]
+		)
     ]
 )
